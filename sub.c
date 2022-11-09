ad16c0ffaa5ac94e4d7002bc41d1b5f90c7bbae0
9400093006 <117814828+9400093006@users.noreply.github.com>
2022-11-09 09:38:39 +0000

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,m,sub;
+printf("enter the value to n");
+scanf("%d",&n);
+printf("enter value of m");
+scanf("%d",&m);
+sub=n-m;
+printf("%d",sub);
+
+return 0;
+}
