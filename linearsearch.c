b4212e109d06f9586bbce6d17d0eafb7c1988abf
9400093006 <117814828+9400093006@users.noreply.github.com>
2022-11-09 10:02:34 +0000

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -10,13 +10,12 @@ int search(int arr[], int N, int x)
 }
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int N = sizeof(arr) / sizeof(arr[0]);
- 
-    int result = search(arr, N, x);
-    (result == -1)
-        ? printf("Element is not present in array")
-        : printf("Element is present at index %d", result);
-    return 0;
+ int arr[] = { 2, 3, 4, 10, 40 };
+  int x = 10;
+  int N = sizeof(arr) / sizeof(arr[0]);
+  int result = search(arr, N, x);
+   (result == -1)
+printf("Element is not present in array")
+printf("Element is present at index %d", result);
+return 0;
 }
